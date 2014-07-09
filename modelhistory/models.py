ed09ccfe0fc4c1b29e4df391a05459e1276699c2
Maxim Oransky <maxim.oransky@gmail.com>
2014-07-09 17:48:42 +0400

    misprint

--- modelhistory/models.py
+++ modelhistory/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.utils.module_loading import import_by_path
 
 
-serializer_name = settings.getattr('MODELHISTORY_SERIALIZER', 'modelhistory.serializers.JsonSerializer')
+serializer_name = getattr(settings, 'MODELHISTORY_SERIALIZER', 'modelhistory.serializers.JsonSerializer')
 serializer = import_by_path(serializer_name)()
 
 
